78401b66b26127ddfbffdb55354670c45d66ac27
chrismsimpson <chris@victoryonemedia.com>
2015-01-10 11:23:24 +1100

    Update SQLiteDatabase.swift
    Add beginTransaction(), commitTransaction() and getErrorMessage()

--- SQLiteDatabase.swift
+++ SQLiteDatabase.swift
@@ -8,6 +8,12 @@
 
 import Foundation
 
+let blankDatabaseFilename:String = "Blank.sqlite"
+
+let instanceDatabaseFilename:String = "Instance.sqlite"
+
+var _SQLiteDatabase:SQLiteDatabase?
+
 class SQLiteDatabase : NSObject {
     
     var cDb:COpaquePointer = nil
@@ -21,6 +27,32 @@ class SQLiteDatabase : NSObject {
         var cFilename = filename.cStringUsingEncoding(NSUTF8StringEncoding)
         
         return SQLiteStatusCode(rawValue: sqlite3_open(cFilename!, &self.cDb))!
+    }
+    
+    // MARK: Transaction
+    
+    func beginTransaction() -> SQLiteStatusCode {
+        
+        var cSqlQuery = "BEGIN TRANSACTION".cStringUsingEncoding(NSUTF8StringEncoding)
+        
+        var cStatusCode = sqlite3_exec(self.cDb, cSqlQuery!, nil, nil, nil)
+        
+        return SQLiteStatusCode(rawValue: cStatusCode)!
+    }
+    
+    func commitTransaction() -> SQLiteStatusCode {
+        
+        var cSqlQuery = "COMMIT TRANSACTION".cStringUsingEncoding(NSUTF8StringEncoding)
+        
+        var cStatusCode = sqlite3_exec(self.cDb, cSqlQuery!, nil, nil, nil)
+        
+        return SQLiteStatusCode(rawValue: cStatusCode)!
+    }
+    
+    // MARK: Error
+    
+    func getErrorMessage() -> String? {
+        return String.fromCString(sqlite3_errmsg(self.cDb))
     }
 }
 
